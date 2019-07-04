2ea1ad3f8b6a08d40c9e4f84b78ecbaa9af8f27f
Madeline Jarvis-Cross <51466332+MadelineJC@users.noreply.github.com>
2019-07-03 20:18:13 -0700

    Create July.3.2019_SeaLiceDatBPbyMonth.R

---
+++ July.3.2019_SeaLiceDatBPbyMonth.R
@@ -0,0 +1,11 @@
+BurdwoodData$Month <- as.factor(BurdwoodData$Month)
+ByMonthGraph<-ggplot(BurdwoodData, aes(Month, AverageLsalmonisfemalesperfish))+
+  geom_boxplot(size=0.5,
+               color=c("magenta","springgreen","turquoise","thistle","skyblue","cornflowerblue","salmon","plum","orangered","palevioletred","orchid","lightgoldenrod"),
+               outlier.shape=4,
+               outlier.size=3)+ 
+  scale_x_discrete(name ="Month", 
+                   labels=c("January","February","March","April","May","June","July","August","September","October","November","December"))+
+  labs(title="Lice Abundance on Fish by Month, 2011-2019", 
+       y="Average L. salmonis females per fish")
+ByMonthGraph
